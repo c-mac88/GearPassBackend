c7423f4ab62349188e190d3ec72ca8e2d8b0d48e
c-mac88 <christianmcfarland@gmail.com>
2016-06-28 10:43:51 -0700

    added some stripe payment functions

--- cloud/main.js
+++ cloud/main.js
@@ -1,58 +1,90 @@
 var API_KEY = "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";
 
-var sendgrid = require('sendgrid').SendGrid(API_KEY);
+var tokenId;
+
+var memberNumber;
+
+var users = new Parse.Query('User');
+users.count({
+    success: function(response) {
+
+        if (response < 10) {
+            memberNumber = "00000" + response;
+        } else if (response < 100) {
+            memberNumber = "0000" + response;
+        } else if (response < 1000) {
+            memberNumber = "000" + response;
+        } else {
+            memberNumber = "00" + response;
+        };
+    },
+    error: function(error) {
+        console.log("error getting membership number");
+    }
+});
 
 Parse.Cloud.define('generateMembershipNumber', function(req, res) {
 
+    var currentUser = req.user;
 
-    var memberNumber;
+    console.log(memberNumber);
+    currentUser.set('membership_number', memberNumber); // figure out a way to generate a random membership number
 
-    var users = new Parse.Query('User');
-    users.count({
-        success: function(response) {
-
-            if (response < 10) {
-                memberNumber = "00000" + response;
-            } else if (response < 100) {
-                memberNumber = "0000" + response;
-            } else if (response < 1000) {
-                memberNumber = "000" + response;
-            } else {
-                memberNumber = "00" + response;
-            }
-
-            var currentUser = req.user;
-            console.log(memberNumber);
-            currentUser.set('membership_number', memberNumber); // figure out a way to generate a random membership number
-            currentUser.set('membership_number_confirmed', false)
-
-            Parse.Object.saveAll([currentUser], { useMasterKey: true }).then(
-                function(response) {
-                    console.log(currentUser.get('email'));
-                    var email = new sendgrid.Email();
-                    email.addTo(currentUser.get('email'));
-                    email.setFrom('noreply@gearpass.com');
-                    email.setSubject("Membership Confirmation");
-                    email.setHtml('Here is your new membership number for GearPass: ' + currentUser.get('membership_number'));
-
-                    console.log(email);
-                    sendgrid.send(email);
-
-                    //     function(err, json) {
-                    //     if (err) {
-                    //         return res.error('Error sending email via SendGrid');
-                    //     }
-                    //     res.success('An email has been sent with your membership number.');
-                    // });
-                },
-                function(err) {
-                    console.log(err);
-                }
-            );
-
+    Parse.Object.saveAll([currentUser], { useMasterKey: true }).then(
+        function(response) {
+            console.log(response);
+            console.log("response");
         },
-        error: function(error) {
-            console.log("error getting membership number");
+        function() {
+            console.log("whoops");
         }
+    );
+
+
+    var helper = require('sendgrid').mail
+    from_email = new helper.Email("noreply@gearpass.com")
+    to_email = new helper.Email(currentUser.get('email'))
+    subject = "GearPass Membership"
+    content = new helper.Content("text/plain", "Here is your new membership number for GearPass: " + currentUser.get('membership_number'))
+    mail = new helper.Mail(from_email, subject, to_email, content)
+
+    var sg = require('sendgrid').SendGrid(API_KEY)
+    var requestBody = mail.toJSON()
+    var request = sg.emptyRequest()
+    request.method = 'POST'
+    request.path = '/v3/mail/send'
+    request.body = requestBody
+    sg.API(request, function(response) {
+        console.log(response.statusCode)
+        console.log(response.body)
+        console.log(response.headers)
+    })
+});
+
+Parse.Cloud.define('collectPaymentInformation', function(req, res) {
+
+    var stripe = require("stripe")("pk_test_qVOqOhnaihK12calHXo8wgQM");
+    var stripeSK = require("stripe")("sk_test_IgaqbKa01DwCOGvS5AzI1MH8");
+    stripe.tokens.create({
+        card: {
+            "number": req.params.number,
+            "exp_month": req.params.month,
+            "exp_year": req.params.year,
+            "cvc": req.params.cvc
+        }
+    }, function(err, token) {
+        console.log(token.id);
+        tokenId = token.id;
     });
+
+    console.log(tokenId);
+
+    stripeSK.customers.create({
+        description: 'new GearPass customer',
+        source: tokenId // obtained with Stripe.js
+    }, function(err, customer) {
+        return customer;
+    });
+
+
 });
--- index.js
+++ index.js
@@ -7,17 +7,20 @@ var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
-
 if (!databaseUri) {
     console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-    databaseURI: databaseUri || 'mongodb://localhost:27017/test',
+    databaseURI: databaseUri || 'mongodb://localhost:27017/GearPass',
     cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
     appId: process.env.APP_ID || '3fb0ee849bd515e37bd07f69771f5d47',
     masterKey: process.env.MASTER_KEY || '662f22623934b6da38c191f1506d96cf', //Add your master key here. Keep it secret!
     serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+    // emailAdapter: SimpleSendGridAdapter({
+    //     apiKey: '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',
+    //     fromAddress: 'noreply@gearpass.com',
+    // }),
     liveQuery: {
         classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
     },
